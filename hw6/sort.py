dacfa9c77ea8f694198968c820c7afb1332b48b0
Volodymyr Kurov <vokur13@gmail.com>
2023-04-27 16:12:11 +0300

    Update sort.py

--- hw6/sort.py
+++ hw6/sort.py
@@ -97,9 +97,9 @@ def handle_output(output):
 
     print(f"Target Directory Items Dict: {output_dict}")
     print(
-        f"Target Directory Known Extentions List: {output_ext}",
+        f"Target Directory Known Extentions List: {set(output_ext)}",
     )
-    print(f"Target Directory Alien Extentions List: {alien_ext}")
+    print(f"Target Directory Alien Extentions List: {set(alien_ext)}")
 
 
 def sort(path):
